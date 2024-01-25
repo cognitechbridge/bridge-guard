20707cb8019780e553050e921c0ae52af72eae27
mh.saadatfar <mh.saadatfar@gmail.com>
2024-01-24 19:31:03 -0800

    Beautify

--- persist/keystore.go
+++ persist/keystore.go
@@ -12,20 +12,25 @@ var _ keystore.KeyStorePersist = (*SqlLiteConnection)(nil)
 
 // SaveKey saves a serialized key in the database
 func (conn *SqlLiteConnection) SaveKey(serializedKey keystore.SerializedKey) error {
-	_, err := conn.dbConn.Exec("INSERT INTO keystore (id, nonce, key, tag) VALUES (?, ?, ?, ?)",
-		serializedKey.ID, serializedKey.Nonce, serializedKey.Key, serializedKey.Tag)
+	_, err := conn.dbConn.Exec(
+		"INSERT INTO keystore (id, nonce, key, tag) VALUES (?, ?, ?, ?)",
+		serializedKey.ID, serializedKey.Nonce, serializedKey.Key, serializedKey.Tag,
+	)
 	return err
 }
 
 // GetKey retrieves a serialized key by its ID
 func (conn *SqlLiteConnection) GetKey(keyID string) (*keystore.SerializedKey, error) {
 	var sk keystore.SerializedKey
-	err := conn.dbConn.QueryRow("SELECT id, nonce, key, tag FROM keystore WHERE id = ?",
-		keyID).Scan(&sk.ID, &sk.Nonce, &sk.Key, &sk.Tag)
+	row := conn.dbConn.QueryRow(
+		"SELECT id, nonce, key, tag FROM keystore WHERE id = ?",
+		keyID,
+	)
+	err := row.Scan(&sk.ID, &sk.Nonce, &sk.Key, &sk.Tag)
 
 	switch {
 	case errors.Is(err, sql.ErrNoRows):
-		return nil, nil
+		return nil, fmt.Errorf("key not found in database (key): %s", keyID)
 	case err != nil:
 		return nil, fmt.Errorf("query failed: %v", err)
 	default:
@@ -36,12 +41,15 @@ func (conn *SqlLiteConnection) GetKey(keyID string) (*keystore.SerializedKey, er
 // GetWithTag retrieves a serialized key by its tag
 func (conn *SqlLiteConnection) GetWithTag(tag string) (*keystore.SerializedKey, error) {
 	var sk keystore.SerializedKey
-	err := conn.dbConn.QueryRow("SELECT id, nonce, key, tag FROM keystore WHERE tag = ?",
-		tag).Scan(&sk.ID, &sk.Nonce, &sk.Key, &sk.Tag)
+	row := conn.dbConn.QueryRow(
+		"SELECT id, nonce, key, tag FROM keystore WHERE tag = ?",
+		tag,
+	)
+	err := row.Scan(&sk.ID, &sk.Nonce, &sk.Key, &sk.Tag)
 
 	switch {
 	case errors.Is(err, sql.ErrNoRows):
-		return nil, nil
+		return nil, fmt.Errorf("key not found in database (tag): %s", tag)
 	case err != nil:
 		return nil, fmt.Errorf("query failed: %v", err)
 	default:
