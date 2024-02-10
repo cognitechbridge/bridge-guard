ad411290c6772e53088e78d5fbb62efde428d838
mh.saadatfar <mh.saadatfar@gmail.com>
2024-02-09 20:21:22 -0800

    Save keys using ChaCha20 and key derivation

--- keystore/serialize.go
+++ keystore/serialize.go
@@ -4,62 +4,77 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
-	"crypto/x509"
 	"ctb-cli/types"
-	"encoding/pem"
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"golang.org/x/crypto/chacha20poly1305"
+	"golang.org/x/crypto/hkdf"
+	"io"
+	"strings"
+)
+
+const (
+	info = "cognitechbridge.com/v1/X25519"
 )
 
 // DeserializePrivateKey encrypts and serializes the private key
-func (*KeyStore) DeserializePrivateKey(ciphered []byte, rootKey *types.Key) (*rsa.PrivateKey, error) {
-	block, _ := pem.Decode(ciphered)
-	if block == nil {
-		return nil, errors.New("failed to parse PEM block containing the key")
+func (*KeyStore) DeserializePrivateKey(serialized []byte, rootKey *types.Key) ([]byte, error) {
+	parts := strings.Split(string(serialized), "\n")
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("invalid serialized key)")
+	}
+	salt, err1 := base64.RawStdEncoding.DecodeString(parts[0])
+	ciphered, err2 := base64.RawStdEncoding.DecodeString(parts[1])
+	if errors.Join(err1, err2) != nil {
+		return nil, fmt.Errorf("invalid serialized key")
 	}
 
-	aead, err := chacha20poly1305.New(rootKey[:])
+	derivedKey, err := deriveKey(rootKey, salt)
+
+	aead, err := chacha20poly1305.New(derivedKey[:])
 	if err != nil {
 		return nil, fmt.Errorf("failed to create cipher: %w", err)
 	}
 
 	nonce := make([]byte, chacha20poly1305.NonceSize)
 
-	deciphered, err := aead.Open(nil, nonce, block.Bytes, nil)
+	deciphered, err := aead.Open(nil, nonce, ciphered, nil)
 	if err != nil {
 		return nil, fmt.Errorf("failed to decrypt data: %w", err)
 	}
 
-	privateKey, err := x509.ParsePKCS1PrivateKey(deciphered)
-	if err != nil {
-		return nil, err
-	}
-
-	return privateKey, nil
+	return deciphered, nil
 }
 
 // SerializePrivateKey encrypts and serializes the private key
-func (*KeyStore) SerializePrivateKey(privateKey *rsa.PrivateKey, rootKey *types.Key) ([]byte, error) {
-	bytes := x509.MarshalPKCS1PrivateKey(privateKey)
+func (*KeyStore) SerializePrivateKey(privateKey []byte, rootKey *types.Key) ([]byte, error) {
+	salt := make([]byte, 16)
+	rand.Read(salt)
 
-	aead, err := chacha20poly1305.New(rootKey[:])
+	derivedKey, err := deriveKey(rootKey, salt)
+
+	aead, err := chacha20poly1305.New(derivedKey[:])
 	if err != nil {
 		return nil, fmt.Errorf("failed to create cipher: %w", err)
 	}
 
 	nonce := make([]byte, chacha20poly1305.NonceSize)
+	ciphered := aead.Seal(nil, nonce, privateKey, nil)
 
-	ciphered := aead.Seal(nil, nonce, bytes, nil)
-
-	pemBytes := pem.EncodeToMemory(
-		&pem.Block{
-			Type:  "RSA PRIVATE KEY",
-			Bytes: ciphered,
-		},
+	res := fmt.Sprintf("%s\n%s",
+		base64.RawStdEncoding.EncodeToString(salt),
+		base64.RawStdEncoding.EncodeToString(ciphered),
 	)
 
-	return pemBytes, nil
+	return []byte(res), nil
+}
+
+func deriveKey(rootKey *types.Key, salt []byte) (derivedKey types.Key, err error) {
+	hkdf := hkdf.New(sha256.New, rootKey[:], salt, []byte(info))
+	derivedKey = types.Key{}
+	_, err = io.ReadFull(hkdf, derivedKey[:])
+	return
 }
 
 // SerializeDataKey encrypts and serializes the key pair
