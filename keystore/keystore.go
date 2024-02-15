ac730aeebd01cf42bb06ca1b03eb2d7fdca8685c
mh.saadatfar <mh.saadatfar@gmail.com>
2024-02-14 22:43:32 -0800

    Make keystorer a single file

--- keystore/keystore.go
+++ keystore/keystore.go
@@ -1,12 +1,14 @@
 package keystore
 
 import (
+	"crypto/rand"
 	"ctb-cli/crypto/key_crypto"
 	"ctb-cli/crypto/recovery"
 	"ctb-cli/filesyetem/key_repository"
 	"ctb-cli/types"
 	"fmt"
 	"golang.org/x/crypto/curve25519"
+	"io"
 	"os"
 )
 
@@ -111,6 +113,48 @@ func (ks *KeyStoreDefault) AddRecoveryKey(inPath string) error {
 	ks.recoveryItems = append(ks.recoveryItems, *rec)
 
 	return nil
+}
+
+func (ks *KeyStoreDefault) LoadKeys() error {
+
+	if ks.privateKey != nil {
+		return nil
+	}
+	serializedPrivateKey, err := ks.keyRepository.GetPrivateKey()
+	if err != nil {
+		return err
+	}
+	ks.privateKey, err = key_crypto.OpenPrivateKey(serializedPrivateKey, &ks.rootKey)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ks *KeyStoreDefault) GenerateClientKeys() (err error) {
+	//Generate private key
+	privateKey := types.Key{}
+	io.ReadFull(rand.Reader, privateKey[:])
+	//Save private key
+	sealPrivateKey, err := key_crypto.SealPrivateKey(privateKey[:], &ks.rootKey)
+	if err != nil {
+		return err
+	}
+	err = ks.keyRepository.SavePrivateKey(sealPrivateKey)
+	if err != nil {
+		return err
+	}
+
+	publicKey, err := ks.getPublicKey()
+	if err != nil {
+		return err
+	}
+	serializedPublic, err := key_crypto.SerializePublicKey(publicKey)
+	if err != nil {
+		return err
+	}
+	err = ks.keyRepository.SavePublicKey(ks.clintId, serializedPublic)
+	return
 }
 
 func (ks *KeyStoreDefault) getPublicKey() ([]byte, error) {
