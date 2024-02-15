3e304fae1bb0bad66ee8e104575e3e238718a0d8
mh.saadatfar <mh.saadatfar@gmail.com>
2024-02-14 20:43:41 -0800

    Implement object_repository and extend object_service resposiblities

--- filesyetem/object/object_service.go
+++ filesyetem/object/object_service.go
@@ -3,11 +3,16 @@ package object
 import (
 	"ctb-cli/crypto/file_crypto"
 	"ctb-cli/crypto/recovery"
+	"ctb-cli/filesyetem/object_cache"
+	"ctb-cli/filesyetem/object_repository"
 	"ctb-cli/types"
 	"io"
 )
 
 type Service struct {
+	cache        *object_cache.ObjectCache
+	objectRepo   *object_repository.ObjectRepository
+	downloader   Downloader
 	keystoreRepo keystoreRepo
 	clientId     string
 }
@@ -18,11 +23,57 @@ type keystoreRepo interface {
 	GetRecoveryItems() ([]types.RecoveryItem, error)
 }
 
-func NewService(keystoreRepo keystoreRepo, clientId string) Service {
+type Downloader interface {
+	Download(id string, writeAt io.WriterAt) error
+	Upload(reader io.Reader, fileId string) error
+}
+
+func NewService(keystoreRepo keystoreRepo, clientId string, cache *object_cache.ObjectCache, objectRepo *object_repository.ObjectRepository, dn Downloader) Service {
 	return Service{
+		downloader:   dn,
+		cache:        cache,
+		objectRepo:   objectRepo,
 		keystoreRepo: keystoreRepo,
 		clientId:     clientId,
 	}
+}
+
+func (f *Service) Read(id string, buff []byte, ofst int64) (n int, err error) {
+	if f.cache.IsInCache(id) {
+		return f.cache.Read(id, buff, ofst)
+	}
+	if f.objectRepo.IsInRepo(id) == false {
+		err := f.downloadToObject(id, err)
+		if err != nil {
+			return 0, err
+		}
+	}
+	err = f.decryptToCache(id, err)
+	if err != nil {
+		return 0, err
+	}
+
+	return f.cache.Read(id, buff, ofst)
+}
+
+func (f *Service) decryptToCache(id string, err error) error {
+	openObject, _ := f.objectRepo.OpenObject(id)
+	defer openObject.Close()
+	decryptedReader, _ := f.Decrypt(openObject, id)
+	writer, err := f.cache.CacheObjectWriter(id)
+	defer writer.Close()
+	_, err = io.Copy(writer, decryptedReader)
+	return err
+}
+
+func (f *Service) downloadToObject(id string, err error) error {
+	file, _ := f.objectRepo.CreateFile(id)
+	err = f.downloader.Download(id, file)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (f *Service) Encrypt(writer io.Writer, fileId string) (write io.WriteCloser, err error) {
---
+++ filesyetem/object_repository/object_repository.go
@@ -0,0 +1,37 @@
+package object_repository
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+)
+
+type ObjectRepository struct {
+	rootPath string
+}
+
+func NewObjectRepository(rootPath string) ObjectRepository {
+	return ObjectRepository{
+		rootPath: rootPath,
+	}
+}
+
+func (o *ObjectRepository) IsInRepo(id string) (is bool) {
+	p := filepath.Join(o.rootPath, id)
+	if _, err := os.Stat(p); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func (o *ObjectRepository) CreateFile(id string) (*os.File, error) {
+	path := filepath.Join(o.rootPath, id)
+	file, _ := os.Create(path)
+	return file, nil
+}
+
+func (o *ObjectRepository) OpenObject(id string) (io.ReadCloser, error) {
+	path := filepath.Join(o.rootPath, id)
+	file, _ := os.Open(path)
+	return file, nil
+}
