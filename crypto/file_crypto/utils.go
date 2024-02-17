49367f605e2cd39cbfdba52c59caf9c34d86c146
mh.saadatfar <mh.saadatfar@gmail.com>
2024-02-16 20:01:30 -0800

    Implement NewKeyFromRand

--- crypto/file_crypto/utils.go
+++ crypto/file_crypto/utils.go
@@ -1,36 +1,5 @@
 package file_crypto
 
-import (
-	"ctb-cli/types"
-	"golang.org/x/crypto/chacha20poly1305"
-	"math/big"
-)
-
-const (
-	lastChunkFlag = 0x01
-)
-
-// nonce represents a key for ChaCha20-Poly1305.
-type key = types.Key
-
-// nonce represents a nonce for ChaCha20-Poly1305.
-type nonce [chacha20poly1305.NonceSize]byte
-
-func getOverHeadSize() int {
-	return chacha20poly1305.Overhead
-}
-
 func getAlgorithmName() string {
 	return "AEAD_ChaCha20_Poly1305"
-}
-
-func (nc *nonce) setLastChunkFlag() {
-	nc[len(nc)-1] = lastChunkFlag
-}
-
-func (nc *nonce) increaseBe() {
-	number := new(big.Int).SetBytes(nc[:])
-	number.Add(number, big.NewInt(1))
-	newBytes := number.Bytes()
-	copy(nc[len(nc)-len(newBytes):], newBytes)
 }
