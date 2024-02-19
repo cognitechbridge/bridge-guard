3f9ec475be67f1ebf668c8b7b68fcb7e83a65ecc
mh.saadatfar <mh.saadatfar@gmail.com>
2024-02-18 16:56:59 -0800

    Use bech32 for recipient

--- repositories/recipient_repository.go
+++ repositories/recipient_repository.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"ctb-cli/types"
 	"encoding/json"
+	"errors"
 	"github.com/samber/lo"
 	"io"
 	"os"
@@ -11,7 +12,12 @@ import (
 
 type RecipientRepository interface {
 	GetRecipientByEmail(email string) (types.Recipient, error)
+	InsertRecipient(recipient types.Recipient) error
 }
+
+var (
+	ErrorWritingToRepository = errors.New("error writing to recipient repo")
+)
 
 // RecipientRepositoryFile File implementation of RecipientRepository
 // @Todo: This implementation is not safe
@@ -25,14 +31,35 @@ func NewRecipientRepositoryFile(rootPath string) *RecipientRepositoryFile {
 	}
 }
 
-func (r *RecipientRepositoryFile) GetRecipientByEmail(email string) (types.Recipient, error) {
+func (r *RecipientRepositoryFile) InsertRecipient(recipient types.Recipient) error {
+	list, _ := r.openRepository()
+	list = append(list, recipient)
+	res, _ := json.Marshal(list)
+
 	path := filepath.Join(r.rootPath, "recipients.txt")
-	file, _ := os.Open(path)
-	data, _ := io.ReadAll(file)
-	var list []types.Recipient
-	_ = json.Unmarshal(data, &list)
+	file, _ := os.OpenFile(path, os.O_RDWR, 0666)
+	defer file.Close()
+	_, err := file.WriteString(string(res))
+	if err != nil {
+		return ErrorWritingToRepository
+	}
+	return nil
+}
+
+func (r *RecipientRepositoryFile) GetRecipientByEmail(email string) (types.Recipient, error) {
+	list, _ := r.openRepository()
 	rec, _ := lo.Find(list, func(r types.Recipient) bool {
 		return r.Email == email
 	})
 	return rec, nil
+}
+
+func (r *RecipientRepositoryFile) openRepository() ([]types.Recipient, error) {
+	path := filepath.Join(r.rootPath, "recipients.txt")
+	file, _ := os.Open(path)
+	defer file.Close()
+	data, _ := io.ReadAll(file)
+	var list []types.Recipient
+	_ = json.Unmarshal(data, &list)
+	return list, nil
 }
--- types/recipient.go
+++ types/recipient.go
@@ -1,7 +1,45 @@
 package types
 
+import (
+	"crypto/rand"
+	"ctb-cli/crypto/bech32"
+	"errors"
+	"io"
+)
+
 type Recipient struct {
 	Email    string `json:"email,omitempty"`
 	Public   string `json:"public,omitempty"`
 	ClientId string `json:"clientId,omitempty"`
+}
+
+var (
+	InvalidRecipient         = errors.New("invalid recipient address")
+	ErrorGeneratingRecipient = errors.New("error generating recipient")
+)
+
+func NewRecipient(email string, public []byte, clientId string) (Recipient, error) {
+	publicStr, err := bech32.Encode("ctb", public)
+	if err != nil {
+		return Recipient{}, err
+	}
+	return Recipient{Email: email, Public: publicStr, ClientId: clientId}, nil
+}
+
+func GenerateRandomRecipient() (string, error) {
+	add := make([]byte, 32)
+	io.ReadFull(rand.Reader, add)
+	res, err := bech32.Encode("ctb", add)
+	if err != nil {
+		return "", ErrorGeneratingRecipient
+	}
+	return res, nil
+}
+
+func (r Recipient) GetPublicBytes() ([]byte, error) {
+	hrp, data, err := bech32.Decode(r.Public)
+	if err != nil || hrp != "ctb" {
+		return nil, InvalidRecipient
+	}
+	return data, nil
 }
