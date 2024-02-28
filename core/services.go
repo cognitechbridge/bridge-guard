227cf1f1ca8ab238533292aabf4a1f4804b70013
mh.saadatfar <mh.saadatfar@gmail.com>
2024-02-27 22:31:15 -0800

    Move filesystem service interface to core

--- core/services.go
+++ core/services.go
@@ -1,5 +1,7 @@
 package core
 
+import "io/fs"
+
 type ObjectService interface {
 	Read(id string, buff []byte, ofst int64) (int, error)
 	Write(id string, buff []byte, ofst int64) (int, error)
@@ -8,4 +10,16 @@ type ObjectService interface {
 	Truncate(id string, size int64) error
 	IsInQueue(id string) bool
 	GetKeyIdByObjectId(id string) (string, error)
+}
+
+type FileSystemService interface {
+	GetSubFiles(path string) (res []fs.FileInfo, err error)
+	CreateFile(path string) (err error)
+	CreateDir(path string) (err error)
+	RemoveDir(path string) (err error)
+	Write(path string, buff []byte, ofst int64) (n int, err error)
+	Read(path string, buff []byte, ofst int64) (n int, err error)
+	Rename(oldPath string, newPath string) (err error)
+	RemovePath(path string) (err error)
+	Resize(path string, size int64) (err error)
 }
