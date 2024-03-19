5c7a100b50e30016791be06d6b522af71705bb48
Mohammad Saadatfar <mh.saadatfar@gmail.com>
2024-03-18 23:02:19 -0700

    Merge init app and init repo commands

--- app/inits.go
+++ app/inits.go
@@ -95,3 +95,14 @@ func SetAndCheckPrivateKey(encodedPrivateKey string) core.AppResult {
 	}
 	return core.AppOkResult()
 }
+
+// InitRepo initializes the repository.
+// It creates a vault in the root path.
+// Returns an AppResult indicating the success or failure of the operation.
+func InitRepo() core.AppResult {
+	// Create a vault in the root path
+	if err := fileSystem.CreateVaultInPath("/"); err != nil {
+		core.AppErrorResult(err)
+	}
+	return core.AppOkResult()
+}
