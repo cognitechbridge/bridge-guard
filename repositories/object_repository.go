f726e5c45c4cb0256a5e98464b9970baee36e41f
Mohammad Saadatfar <mh.saadatfar@gmail.com>
2024-04-21 21:35:54 -0700

    Use dir path for accessing objects

--- repositories/object_repository.go
+++ repositories/object_repository.go
@@ -16,27 +16,35 @@ func NewObjectRepository(rootPath string) ObjectRepository {
 	}
 }
 
-func (o *ObjectRepository) IsInRepo(id string) (is bool) {
-	p := filepath.Join(o.rootPath, id)
+func (o *ObjectRepository) IsInRepo(id string, dir string) (is bool) {
+	p := o.GetPath(id, dir)
 	if _, err := os.Stat(p); os.IsNotExist(err) {
 		return false
 	}
 	return true
 }
 
-func (o *ObjectRepository) CreateFile(id string) (*os.File, error) {
-	path := filepath.Join(o.rootPath, id)
+func (o *ObjectRepository) CreateFile(id string, dir string) (*os.File, error) {
+	path := o.GetPath(id, dir)
+	//@TODO: Remove this line
+	os.MkdirAll(filepath.Dir(path), os.ModePerm)
 	file, _ := os.Create(path)
 	return file, nil
 }
 
-func (o *ObjectRepository) OpenObject(id string) (io.ReadCloser, error) {
-	path := filepath.Join(o.rootPath, id)
+func (o *ObjectRepository) OpenObject(id string, dir string) (io.ReadCloser, error) {
+	path := o.GetPath(id, dir)
 	file, _ := os.Open(path)
 	return file, nil
 }
 
-func (o *ObjectRepository) GetPath(id string) (string, error) {
-	path := filepath.Join(o.rootPath, id)
-	return path, nil
+func (o *ObjectRepository) ChangeDir(id string, oldDir string, newDir string) error {
+	oldPath := o.GetPath(id, oldDir)
+	newPath := o.GetPath(id, newDir)
+	return os.Rename(oldPath, newPath)
+}
+
+func (o *ObjectRepository) GetPath(id string, dir string) string {
+	path := filepath.Join(o.rootPath, dir, id)
+	return path
 }
--- services/object_service/types.go
+++ services/object_service/types.go
@@ -5,10 +5,12 @@ import "ctb-cli/core"
 // encryptChanItem represents an item to be encrypted.
 type encryptChanItem struct {
 	id  string
+	dir string
 	key *core.KeyInfo
 }
 
 // uploadChanItem represents an item to be uploaded.
 type uploadChanItem struct {
-	id string
+	id  string
+	dir string
 }
