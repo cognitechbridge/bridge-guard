ad9d2b0c5dedf8d93da5d66e7dc5c98c30e9f085
Mohammad Saadatfar <mh.saadatfar@gmail.com>
2024-05-20 11:32:16 -0700

    Implement core public key and private key structs

--- core/key.go
+++ core/key.go
@@ -6,6 +6,73 @@ import (
 	"golang.org/x/crypto/curve25519"
 )
 
+type PublicKey []byte
+
+// NewPublicKeyFromEncoded creates a PublicKey from an encoded base58 string.
+func NewPublicKeyFromEncoded(encoded string) (PublicKey, error) {
+	if len(encoded) != 44 {
+		return nil, ErrInvalidPublicKey
+	}
+	return DecodePublic(encoded)
+}
+
+// NewPublicKeyFromBytes creates a PublicKey from a byte slice.
+func NewPublicKeyFromBytes(bytes []byte) PublicKey {
+	return PublicKey(bytes)
+}
+
+// String returns the base58 encoded string representation of the PublicKey.
+func (key PublicKey) String() string {
+	return key.Encode()
+}
+
+// Encode returns the base58 encoded string representation of the PublicKey.
+func (key PublicKey) Encode() string {
+	return EncodePublic(key)
+}
+
+// Bytes returns the byte slice representation of the PublicKey.
+func (key PublicKey) Bytes() []byte {
+	return []byte(key)
+}
+
+// Equals returns true if the PublicKey is equal to the other PublicKey.
+func (key PublicKey) Equals(other PublicKey) bool {
+	return key.String() == other.String()
+}
+
+type PrivateKey []byte
+
+// NewPrivateKeyFromEncoded creates a PrivateKey from an encoded base58 string.
+func NewPrivateKeyFromEncoded(encoded string) (PrivateKey, error) {
+	return DecodePrivateKey(encoded)
+}
+
+// NewPrivateKeyFromBytes creates a PrivateKey from a byte slice.
+func NewPrivateKeyFromBytes(bytes []byte) PrivateKey {
+	return PrivateKey(bytes)
+}
+
+// String returns the base58 encoded string representation of the PrivateKey.
+func (key PrivateKey) String() string {
+	return key.Encode()
+}
+
+// Encode returns the base58 encoded string representation of the PrivateKey.
+func (key PrivateKey) Encode() string {
+	return encodedPrivateKey(key)
+}
+
+// Bytes returns the byte slice representation of the PrivateKey.
+func (key PrivateKey) Bytes() []byte {
+	return []byte(key)
+}
+
+// Equals returns true if the PrivateKey is equal to the other PrivateKey.
+func (key PrivateKey) Equals(other PrivateKey) bool {
+	return key.String() == other.String()
+}
+
 type UserKeyPair struct {
 	PublicKey  string
 	PrivateKey string
@@ -27,18 +94,12 @@ func GenerateUserKey() (*UserKeyPair, error) {
 	if err != nil {
 		return nil, err
 	}
-	encodedPrivateKey, err := encodedPrivateKey(key)
-	if err != nil {
-		return nil, err
-	}
+	encodedPrivateKey := encodedPrivateKey(key)
 	publicKey, err := curve25519.X25519(key, curve25519.Basepoint)
 	if err != nil {
 		return nil, err
 	}
-	encodedPublicKey, err := EncodePublic(publicKey)
-	if err != nil {
-		return nil, err
-	}
+	encodedPublicKey := EncodePublic(publicKey)
 	return &UserKeyPair{
 		PublicKey:  encodedPublicKey,
 		PrivateKey: encodedPrivateKey,
