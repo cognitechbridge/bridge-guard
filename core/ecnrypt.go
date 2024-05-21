92f4267793f1a1fe04b6d77713c54c5818ed1f25
Mohammad Saadatfar <mh.saadatfar@gmail.com>
2024-05-20 22:38:38 -0700

    Implement functions related to empty key

--- core/ecnrypt.go
+++ core/ecnrypt.go
@@ -29,11 +29,16 @@ func NewKeyFromRand() Key {
 func KeyFromBytes(bytes []byte) (Key, error) {
 	// Check if the slice length is exactly 3
 	if len(bytes) != chacha20poly1305.KeySize {
-		return Key{}, errors.New("bytes does not contain exactly required bytes")
+		return EmptyKey(), errors.New("bytes does not contain exactly required bytes")
 	}
 	return Key{
 		value: bytes,
 	}, nil
+}
+
+// EmptyKey returns an empty key
+func EmptyKey() Key {
+	return Key{}
 }
 
 // Bytes returns the key as a byte slice
@@ -44,6 +49,20 @@ func (k *Key) Bytes() []byte {
 // Equals returns true if the key is equal to the other key
 func (k *Key) Equals(other Key) bool {
 	return bytes.Equal(k.value[:], other.value[:])
+}
+
+// IsEmpty returns true if the key is empty
+func (k *Key) IsEmpty() bool {
+	if k.Equals(Key{}) {
+		return true
+	}
+	// Check if all bytes are 0
+	for _, b := range k.value {
+		if b != 0 {
+			return false
+		}
+	}
+	return true
 }
 
 // KeyInfo represents a key and its id
