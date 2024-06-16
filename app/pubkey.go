2edee79618959dca7d1535f307088cd12c012856
Mohammad Saadatfar <mh.saadatfar@gmail.com>
2024-06-15 19:40:28 -0700

    fix: Fix returning public key bug

--- app/pubkey.go
+++ app/pubkey.go
@@ -2,21 +2,18 @@ package app
 
 import (
 	"ctb-cli/core"
+	"ctb-cli/services/key_service"
 )
 
 // GetPubkey generates a public key from a private key.
 func (a *App) GetPubkey(privateKeyString string) core.AppResult {
-	// init the app
-	initRes := a.initServices()
-	if !initRes.Ok {
-		// return the repository status with IsValid = false if the app failed to initialize the services
-		return core.NewAppResultWithValue(core.NewInvalidRepositoyStatus(false))
-	}
 	privateKey, err := core.NewPrivateKeyFromEncoded(privateKeyString)
 	if err != nil {
 		return core.NewAppResultWithError(err)
 	}
-	publicKey, err := a.keyStore.GetPublicKeyByPrivateKey(privateKey)
+	// Create a new key store without key and vault repositories.
+	keyStore := key_service.NewKeyStore(nil, nil)
+	publicKey, err := keyStore.GetPublicKeyByPrivateKey(privateKey)
 	if err != nil {
 		return core.NewAppResultWithError(err)
 	}
